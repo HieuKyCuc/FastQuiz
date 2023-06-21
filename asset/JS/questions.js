9c545e008ce76fc535b43f6a6ccb88f5ef4cdac7
MinhHieu <minhhieu77886@gmail.com>
2023-06-22 00:10:14 +0700

    Add new project food

---
+++ asset/JS/questions.js
@@ -0,0 +1,42 @@
+let question = [
+  {
+    numb: 1,
+    question: "Conga là gì?",
+    answer: "A. Con gà",
+    option: ["A. Con gà", "B. Con gá", "C. Con gả", "D. Con ga"],
+  },
+  {
+    numb: 2,
+    question: "Ngày tháng năm sinh của aiu?",
+    answer: "C. 05/12/1999",
+    option: [
+      "A. 12/05/1999",
+      "B. 12/05/1998",
+      "C. 05/12/1999",
+      "D. 05/12/1998",
+    ],
+  },
+  {
+    numb: 3,
+    question: "Bé có iu a hông?",
+    answer: "C. Iu Anh",
+    option: ["A. Có", "B. Nồ", "C. Iu Anh", "D. Chê"],
+  },
+  {
+    numb: 4,
+    question: "Bé iu muốn ăn gì?",
+    answer: "C. Aiu",
+    option: ["A. Bimbim", "B. Lẩu", "C. Aiu", "D. Hông ăn"],
+  },
+  {
+    numb: 5,
+    question: "Bé có dễ thương hông?",
+    answer: "D. Siêu dễ thương",
+    option: [
+      "A. Có",
+      "B. Rất dễ thương",
+      "C. Rất rất dễ thương",
+      "D. Siêu dễ thương",
+    ],
+  },
+];
